f62f2d94179a2d8e35a1406c5946b798e40e63bf
Khushboo Baheti <khushboobaheti627@gmail.com>
2017-04-05 01:35:25 +0530

    Profile and Address class created

---
+++ src/Crime/Address.java
@@ -0,0 +1,39 @@
+package Crime;
+
+public class Address {
+	private String city;
+	private String state;
+	private String landmark;
+	private String plot;
+	
+	public String getCity(){
+		return city;
+	}
+	
+	public String getState(){
+		return state;
+	}
+	public String getLandmark(){
+		return landmark;
+	}
+	public String getplot(){
+		return plot;
+	}
+	
+	
+	public void setCity(String c){
+		city=c;
+	}
+	public void setState(String s){
+		state=s;
+	}
+	public void setLandmark(String l){
+		landmark=l;
+	}
+	public void setplot(String p){
+		plot=p;
+	}
+	
+	
+	
+}
---
+++ src/Crime/Profile.java
@@ -0,0 +1,61 @@
+package Crime;
+
+public class Profile {
+		private String name;
+		private String id;
+		private int contact;
+		private Address a;
+		private int age;
+		private String email;
+		private String gender;
+		private String category;
+		
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getId() {
+			return id;
+		}
+		public void setId(String id) {
+			this.id = id;
+		}
+		public int getContact() {
+			return contact;
+		}
+		public void setContact(int contact) {
+			this.contact = contact;
+		}
+		public Address getA() {
+			return a;
+		}
+		public void setA(Address a) {
+			this.a = a;
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getGender() {
+			return gender;
+		}
+		public void setGender(String gender) {
+			this.gender = gender;
+		}
+		public String getCategory() {
+			return category;
+		}
+		public void setCategory(String category) {
+			this.category = category;
+		}
+}
